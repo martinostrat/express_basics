ec5fa48c0c5aedf7472860b226efa9e94486211b
Martin <martin.ostrat@khk.ee>
2022-02-03 11:51:25 +0200

    post request use

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+const body = require('body-parser');
+app.use(body.urlencoded({extended:true}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/questions', (req, res) => {
-    let questions = [
-        {title: 'What is NodeJS', user: 'Kadi', votes: '10'},
-        {title: 'What is ExpressJS', user: 'Madis', votes: '8'},
-    ] 
-    res.render('index', {questions: questions});
+app.get('/login', (req, res) => {
+    res.render('login');
 });
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if(username === 'user' && password === 'qwerty') {
+        res.redirect('/dashboard');
+    }
+})
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+})
 
 app.listen(3000, () => {
     console.log('server started at localhost:3000');
