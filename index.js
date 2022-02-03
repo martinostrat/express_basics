495416f735219efcbeb53d4dbab94947f4d8fdfb
Martin <martin.ostrat@khk.ee>
2022-02-03 11:21:38 +0200

    Templating dir setup

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res) => {
     let user = req.params.username;
-    res.render('index.ejs', {username : user});
+    res.render('index', {username : user});
 });
 
 app.listen(3000, () => {
